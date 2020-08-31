99142b96aeee410ed60aded797efdd66fbbd4653
Carl-u <2249403337@qq.com>
2020-09-01 07:44:30 +0800

    第一次测试Git使用的记录

---
+++ gdms-web/src/com/oracle/gdms/entity/GoodsType.java
@@ -0,0 +1,44 @@
+package com.oracle.gdms.entity;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class GoodsType implements Serializable {
+
+	private Integer gtid;
+	private String name;
+	private String description;
+	private String Area;
+
+	public Integer getGtid() {
+		return gtid;
+	}
+
+	public void setGtid(Integer gtid) {
+		this.gtid = gtid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getArea() {
+		// TODO 自动生成的方法存根
+		return Area;
+	}
+
+}
